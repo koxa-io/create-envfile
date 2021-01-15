6de19cd3a739e94196fc372d496a8685dd6693e9
rajeevnaikte <rajeevnaikte@gmail.com>
2021-01-15 17:07:24 -0500

    use current working directory

--- src/create-envfile.py
+++ src/create-envfile.py
@@ -16,7 +16,7 @@ directory = str(os.environ.get("INPUT_DIRECTORY"))
 # .env is set by default
 file_name = str(os.environ.get("INPUT_FILE_NAME"))
 
-path = "/github/workspace"
+path = os.getcwd()
 
 print("Saving file: " + os.path.join(path, directory, file_name))
 
